68c7681bc8081223b454ad94695e82e927e9253f
DrewRosales <drrosales@gmail.com>
2024-10-12 21:01:26 -0400

    Removed build and log directories

--- midterm/en613_control/build/en613_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/drew/repos/IntroRobotics/midterm/en613_control/install/en613_control'
