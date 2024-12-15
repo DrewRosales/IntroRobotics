0d835e31185884796103ec229ba06f9f28502a3e
DrewRosales <drrosales@gmail.com>
2024-12-15 16:38:13 -0500

    Added weights to the heuristics to make sure the plan does not change directions too often

--- final/robot_spawner_pkg/robot_spawner_pkg/a_star_planner.py
+++ final/robot_spawner_pkg/robot_spawner_pkg/a_star_planner.py
@@ -1,20 +1,11 @@
 import signal
 import sys
-
 import numpy as np
 from typing import List, Tuple
-
 from heapq import heappush, heappop
 import cv2
 
-
 class Edge: 
-    """
-    This class provides a basic data structure for representing
-    a directional edge in a graph. Travel is possible between
-    the starting node to the ending node at the given cost
-    but travel in the opposite direction is not allowed.
-    """
     def __init__(self,starting_node, ending_node, cost):
         self.start = starting_node
         self.end = ending_node 
@@ -26,27 +17,17 @@ class Edge:
         return f'({self.start.name},{self.end.name},{self.cost})'
 
     def __eq__(self,obj):
-        if  isinstance(obj, Edge):
+        if isinstance(obj, Edge):
             return self.start == obj.start and obj.end == obj.end and self.cost == self.cost 
         return False
 
 class Node:
-    """
-    This class provides a basic data structure for representing
-    a node in A* Graph
-    """
     def __init__(self, name, h):
-        #The name of the node (can be anything, just for human readable output)
         self.name = name
-        #The current best cost-to-come for the node
         self.g = np.inf 
-        #The current best estimate of the node's total cost
         self.f = np.inf 
-        #The heuristic estimate of the cost-to-go for the node
         self.h = h 
-        #The list of edges which connect the node 
         self.edges = []
-        #The previous node in path to the goal
         self.previous = None
 
     def add_neighbor(self, node, cost):
@@ -57,13 +38,12 @@ class Node:
         self.add_neighbor(node, cost)
         node.add_neighbor(self, cost)
 
-
     def __str__(self):
         return f'({self.name},{self.f},{self.g},{self.h})'
 
     def __eq__(self,obj):
-        if  isinstance(obj, Node):
-            return self.name == obj.name and self.f == self.f  and obj.g == obj.g and self.h == self.h 
+        if isinstance(obj, Node):
+            return self.name == obj.name and self.f == self.f and obj.g == obj.g and self.h == self.h 
         return False
 
     def __ge__(self, other):
@@ -80,11 +60,37 @@ def create_path_grid(cost_to_come, current):
     path.reverse()
     return path
 
-def heuristic(a, b):
-    return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+def directional_heuristic(current: tuple, next_pos: tuple, goal: tuple, start: tuple) -> float:
+    # Calculate basic Euclidean distance
+    base_h = np.sqrt((next_pos[0] - goal[0]) ** 2 + (next_pos[1] - goal[1]) ** 2)
+    
+    if current == start:
+        return base_h
+        
+    # Calculate desired direction (from start to goal)
+    goal_dx = goal[0] - start[0]
+    goal_dy = goal[1] - start[1]
+    goal_mag = np.sqrt(goal_dx**2 + goal_dy**2)
+    if goal_mag == 0:
+        return base_h
+    goal_dir = (goal_dx/goal_mag, goal_dy/goal_mag)
+    
+    # Calculate actual movement direction
+    move_dx = next_pos[0] - current[0]
+    move_dy = next_pos[1] - current[1]
+    move_mag = np.sqrt(move_dx**2 + move_dy**2)
+    if move_mag == 0:
+        return base_h
+    move_dir = (move_dx/move_mag, move_dy/move_mag)
+    
+    alignment = move_dir[0]*goal_dir[0] + move_dir[1]*goal_dir[1]
+
+    FORWARD_WEIGHT = 0.75
+    direction_factor = 2 - (alignment + 1) * FORWARD_WEIGHT
+    
+    return base_h * direction_factor
 
 def inflate_grid(grid: np.ndarray, robot_radius: float, resolution: float) -> np.ndarray:
-
     # Convert to binary grid (0 for free, 1 for occupied)
     binary_grid = (grid >= 50).astype(np.uint8)
     
@@ -110,7 +116,7 @@ def a_star_grid(map: np.ndarray, start: tuple, goal: tuple) -> List[tuple]:
     heappush(open_set, (0, start))
     cost_to_come = {}
     g = {start: 0}
-    f = {start: heuristic(start, goal)}
+    f = {start: directional_heuristic(start, start, goal, start)}
 
     while open_set:
         _, current = heappop(open_set)
@@ -135,30 +141,12 @@ def a_star_grid(map: np.ndarray, start: tuple, goal: tuple) -> List[tuple]:
             if neighbor not in g or tentative_g < g[neighbor]:
                 cost_to_come[neighbor] = current
                 g[neighbor] = tentative_g
-                f[neighbor] = tentative_g + heuristic(neighbor, goal)
+                f[neighbor] = tentative_g + directional_heuristic(current, neighbor, goal, start)
                 heappush(open_set, (f[neighbor], neighbor))
     
     return []
 
 def a_star_graph(start: Node, goal: Node) -> List[Node]:
-    """
-    This function will compute the optimal path between a starting node and an ending node.
-    The result should be a list of the Edges that represent the optimal path to the goal. 
-    For this function the cost and heuristic functions are defined when the node is originally created.
-
-    
-    If no path exists then this function should return an empty list.
-
-    Worth 50 pts
-    
-    Input
-      :param start: The starting node of the search
-      :param goal: The ending node of the search
-
-    Output
-      :return: path: a list of Node objects representing the optimal path to the goal 
-    """
-
     open_list = []
     heappush(open_list, (0, start))
     start.g = 0
@@ -209,7 +197,6 @@ def graph_demo():
         print(e_i)
 
 def grid_demo():
-
     map = np.array([[0,0,1,0,0,0,0,0,0],
                     [0,0,1,0,0,0,0,0,0],
                     [0,0,1,0,0,1,1,1,0],
